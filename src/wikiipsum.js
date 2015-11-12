2aba5b3a377028d5cf01174978f31a0bd568ef64
Evan Tarrh <evantarrh@gmail.com>
2015-11-12 14:07:15 -0500

    adding license and structure

---
+++ src/wikiipsum.js
@@ -0,0 +1,12 @@
+(function(window) {
+
+  "use strict";
+
+  function Ipsum() {
+    this.elements = document.getElementsByClassName("ipsum");
+    return this;
+  }
+
+  window.Ipsum = Ipsum;
+
+}(window));
